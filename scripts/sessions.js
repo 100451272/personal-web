691614e976b7257c872171f2f31fe602d2349651
100451272 <100451272@alumnos.uc3m.es>
2023-01-10 20:36:56 +0100

    play pause and corrections

--- scripts/sessions.js
+++ scripts/sessions.js
@@ -1,3 +1,16 @@
+
+document.addEventListener('keyup', event => {
+    let audio = document.getElementsByClassName("audio")[0];
+    if (event.code === 'Space') {
+      if (!audio.paused) {
+        audio.pause();
+    }
+    else {
+        audio.play();
+    }
+    }
+})
+
 function play_session(title){
     let audio = document.getElementsByClassName("audio")[0];
     let path = "./media/audio/sessions/" + title + ".mp3";
