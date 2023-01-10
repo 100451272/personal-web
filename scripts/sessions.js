66c0cba0950e471747433201ffc450ccc6297486
100451272 <100451272@alumnos.uc3m.es>
2023-01-10 20:09:29 +0100

    sessions autoplay ok

--- scripts/sessions.js
+++ scripts/sessions.js
@@ -1,10 +1,5 @@
-const audio = document.getElementsByClassName("audio");
-const source = document.getElementById("source");
-
 function play_session(title){
-    console.log(source);
-    let path = "./media/audio/sessions/" + title + ".wav";
-    source.src = path;
-    audio.load();
-    audio.play;
+    let audio = document.getElementsByClassName("audio")[0];
+    let path = "./media/audio/sessions/" + title + ".mp3";
+    audio.src = path;
 }
